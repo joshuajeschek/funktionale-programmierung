b3c28682281a029c95568f051bdf534204c0bd26
Joshua Jeschek <joshua@jeschek.eu>
2022-11-25 10:25:26 +0100

    übung 04 update

--- hs/04.hs
+++ hs/04.hs
@@ -6,10 +6,12 @@ flip f a b = f b a
 -- B)
 total :: (Int -> Int) -> (Int -> Int)
 total f n = sum [f i | i <- [0..n]]
+-- + rekursion
+-- + map function
 
 -- C)
 composeList :: [a -> a] -> (a -> a)
-composeList [] = id
+composeList []     = id
 composeList (f:fs) = composeList fs . f
 
 -- D)
@@ -22,6 +24,7 @@ calcOps fs a b = [f a b | f <- fs]
 
 -- F)
 linearSearch :: (a -> Bool) -> [a] -> Int
+linearSearch _ [] = -1
 linearSearch pred (x:xs)
   | pred x    = 0
   | otherwise = 1 + linearSearch pred xs
@@ -31,7 +34,7 @@ quickSortGen :: (a -> a -> Bool) -> [a] -> [a]
 quickSortGen _ [] = []
 quickSortGen smallerThan (x:xs) = quickSortGen smallerThan [y | y <- xs, y `smallerThan` x] ++
                             [x] ++
-                           quickSortGen smallerThan [y | y <- xs, not(y `smallerThan` x)]
+                           quickSortGen smallerThan [z | z <- xs, not(z `smallerThan` x)]
 
 quickSortStrings :: [String] -> [String]
 quickSortStrings = quickSortGen (<=)
